cce413da5890713f7526bd698ec568cea5ec222e
José Assis de Menezes Neto <jose.neto@hitcode.com.br>
2021-03-29 09:51:58 -0300

    Finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,38 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+      if (newTaskTitle) {
+        const task = {
+          id: Math.floor(Math.random() * 100 + 1),
+          title: newTaskTitle,
+          isComplete: false,
+        }
+        setTasks(prevState => [...prevState, task]);
+      }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const todosModified = tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+
+      return task;
+    })
+
+    setTasks(todosModified)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const todosUpdated = [];
+
+    tasks.forEach(task => {
+      if (task?.id !== id) {
+        todosUpdated.push(task);
+      }
+    })
+
+    setTasks(todosUpdated)
   }
 
   return (
